049041d96614e71fce8167e01c96ab80e5e96e00
RAJ 143 <52394727+prabhakarsarkar@users.noreply.github.com>
2020-10-25 10:40:25 +0530

    Add files via upload

---
+++ public/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+// import FunctionCom from './Component/FunctionCom'
+// import ClassCom from './Component/ClassCom'
+// import States from './Component/States'
+// import SetState from './Component/SetStatea'
+// import Click from './Component/FunClick'
+// import Classclick from './Component/ClassClick'
+// import BindEvent from './Component/BindEvent'
+// import PreantComponent from './Component/PreantComponent'
+// import Conditon from './Component/condition'
+import ListMap from './Component/ListMap'
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <ListMap/>
+
+    {/* <Conditon/> */}
+    {/* <PreantComponent/> */}
+  {/* <BindEvent/> */}
+   {/* <Classclick/> */}
+  {/* <Click/> */}
+    {/* <SetState/> */}
+  {/* <States/> */}
+
+  {/* <FunctionCom name ="raj"/>
+  <ClassCom  name ="panchie"/> */}
+    </div>
+  );
+}
+
+export default App;
